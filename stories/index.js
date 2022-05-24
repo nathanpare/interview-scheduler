31c31b1172b021be575e75ebbcfb975f4f7c9704
Nathan Pare <nathanpare@Nathans-MacBook-Pro.local>
2022-05-23 22:59:45 -0700

    Create the Appointment component

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,10 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem"
 import InterviewerList from "components/InterviewerList"
+import Appointment from "components/Appointment/index.js";
+import Header from "components/Appointment/Header"
+import Empty from "components/Appointment/Empty"
+import Show from "components/Appointment/Show"
 
 storiesOf("DayListItem", module)
   .addParameters({
@@ -132,3 +136,14 @@ storiesOf("Button", module)
             setInterviewer={action("setInterviewer")}
           />
         ));
+
+  storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+  .add("Show", () => <Show student="Lydia Miller-Jones" interviewers={interviewers} onEdit={ action("onEdit")} onDelete={ action("onDelete")}/>);
+
