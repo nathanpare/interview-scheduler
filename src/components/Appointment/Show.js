d415671af74c36492fe8d1c25bbc0410d0218438
Nathan Pare <nathanpare@Nathans-MacBook-Pro.local>
2022-05-24 13:47:01 -0700

    Create the remaining appointment views

--- src/components/Appointment/Show.js
+++ src/components/Appointment/Show.js
@@ -7,7 +7,7 @@ export default function Show(props){
     <h2 className="text--regular">{props.student}</h2>
     <section className="interviewer">
       <h4 className="text--light">Interviewer</h4>
-      <h3 className="text--regular">{props.interviewer}</h3>
+      <h3 className="text--regular">{props.interviewer.name}</h3>
     </section>
   </section>
   <section className="appointment__card-right">
@@ -16,14 +16,14 @@ export default function Show(props){
         className="appointment__actions-button"
         src="images/edit.png"
         alt="Edit"
+        onClick={props.onEdit}
         />
-        {props.onEdit}
       <img
         className="appointment__actions-button"
         src="images/trash.png"
         alt="Delete"
+        onClick={props.onDelete}
       />
-      {props.onDelete}
     </section>
   </section>
 </main>
